3ff2fed1ba4861ed73ac4c48d319cdc2596bca7c
Your Name <you@example.com>
2021-03-19 14:05:56 +0530

    first git commit

---
+++ src/test/java/com/tests/LoginTest.java
@@ -0,0 +1,53 @@
+package com.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+public class LoginTest {
+	WebDriver driver;
+	Logger log=Logger.getLogger(LoginTest.class);
+	
+	@BeforeMethod
+	public void setup(){
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		driver=new ChromeDriver();
+		log.info("launching chrome browser");
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		
+		//Getting the url from config.properties file
+		driver.get("https://www.freecrm.com/");	
+		log.info("entering application url");
+		log.warn("Just warning msg");
+		log.fatal("this is a fatal msg");
+		log.debug("debugger msg");
+	}
+	
+	
+	@Test
+	public void freecrmtitle(){
+		String title=driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals(title,"#1 Free CRM customer relationship management software cloud");
+	}
+	
+	
+	
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+
+}
